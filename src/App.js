9d6276adf81d27584bbb28c5c9d31e632097e097
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-04 15:58:43 -0600

    Update App.js
    -updated light theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./globalStyles"
 
 function App() {
   return <>
 
     <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
     App File
+    </ThemeProvider>
+
+
+    
     </>
     
 }
