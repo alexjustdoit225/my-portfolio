9433a25acb26e4feda7cd7fbdd76548221299425
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-05 18:17:23 -0600

    Update SocialIcons.js

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -5,3 +5,19 @@ import styled from 'styled-components'
 import { Facebook, Github, Twitter, YouTube } from '../components/AllSvgs'
 import {DarkTheme} from '../components/Themes'
 
+const Icons = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+position: fixed;
+bottom: 0;
+left: 2rem;
+
+z-index:3;
+
+&>*:not(:last-child){
+    margin: 0.5rem 0;
+}
+`
+
